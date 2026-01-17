45fc67335d4bc212cd87bfe562476af989427f6c
djunekz <gab288.gab288@passinbox.com>
2026-01-17 10:21:16 +0700

    chore(release): v1.0.1

--- core/version.sh
+++ core/version.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
-# ==============================
-# TDOC Version (Central Source)
-# ==============================
+#!/usr/bin/env bash
+# SINGLE SOURCE OF TRUTH
 
-TDOC_NAME="tdoc"
 TDOC_VERSION="1.0.1"
-TDOC_CODENAME="stable"
-TDOC_BUILD_DATE="2026-01-16"
-
-export TDOC_NAME
-export TDOC_VERSION
-export TDOC_CODENAME
-export TDOC_BUILD_DATE
-
-tdoc_version_string() {
-  echo "$TDOC_NAME v$TDOC_VERSION ($TDOC_CODENAME)"
-}
