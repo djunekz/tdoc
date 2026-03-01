1bebdde998d07c4f8a7bdd2fe02e409e3c76f166
djunekz <gab288.gab288@passinbox.com>
2026-03-02 03:55:36 +0700

    Update version

--- core/version.sh
+++ core/version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export TDOC_NAME="TDOC (Termux Doctor)"
-export TDOC_VERSION="1.0.5"
+export TDOC_VERSION="1.0.6"
 export TDOC_CODENAME="Stabilization"
-export TDOC_BUILD_DATE="2026-01-19"
+export TDOC_BUILD_DATE="2026-03-02"
